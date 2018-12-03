ed6631f7882bf76d1bbe4208c2c7f4730f95c255
CieloSun <cielosun@outlook.com>
2018-12-03 17:39:12 +0800

    change

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone --depth 1 https://github.com/rupa/z.git
-cp .bashrc ~/.bashrc
+cp ~/config/.bashrc ~/.bashrc
 echo -e "# customize\nsource ~/config/.aliases\n# wsl cannot use z directly\nunsetopt BG_NICE\n. ~/config/z/z.sh">>~/.zshrc
 
