62897f0e82030e4484507da1ec4910e437dc819e
CieloSun <cielosun@outlook.com>
2018-12-03 09:50:22 +0000

    finish run

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+apt update
+apt install -y wget git zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+cd ~
 git clone --depth 1 https://github.com/rupa/z.git
 cp ~/config/.bashrc ~/.bashrc
 echo -e "# customize\nsource ~/config/.aliases\n# wsl cannot use z directly\nunsetopt BG_NICE\n. ~/config/z/z.sh">>~/.zshrc
