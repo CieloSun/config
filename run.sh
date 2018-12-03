609e1860ad9a270e57724d1967c870d3df6c2bc2
Cielo Sun <cielosun@outlook.com>
2018-12-03 19:35:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,10 +8,9 @@ git config --global user.name "CieloSun"
 # oh-my-zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 # git clone z
-cd ~
 git clone --depth 1 https://github.com/rupa/z.git
 # copy my bashrc
-echo -e "exec zsh\nsource .zshrc">>~/.bashrc
+echo "exec zsh\nsource .zshrc">>~/.bashrc
 # add context in zshrc
-echo -e "# customize\nsource ~/config/.aliases\n# wsl cannot use z directly\nunsetopt BG_NICE\n. ~/config/z/z.sh">>~/.zshrc
+echo "# customize\nsource ~/config/.aliases\n# wsl cannot use z directly\nunsetopt BG_NICE\n. ~/config/z/z.sh">>~/.zshrc
 
