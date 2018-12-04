2491db114f690e87bd29dc6eb65cd9bd5c58cb17
CieloSun <cielosun@outlook.com>
2018-12-04 18:13:54 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install
 # git clone z
 git clone --depth 1 https://github.com/rupa/z.git
 # copy my bashrc
-echo "exec zsh\nsource .zshrc">>~/.bashrc
+echo -e "exec zsh\nsource .zshrc">>~/.bashrc
 # add context in zshrc
-echo "# customize\nsource ~/config/.aliases\n# wsl cannot use z directly\nunsetopt BG_NICE\n. ~/config/z/z.sh">>~/.zshrc
+echo -e "# customize\nsource ~/config/.aliases\n# wsl cannot use z directly\nunsetopt BG_NICE\n. ~/config/z/z.sh">>~/.zshrc
 
